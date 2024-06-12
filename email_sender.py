d5fbf7c051c11e63e21ff291428c00eea0d40c1e
naan-mxco <naan.mxco@gmail.com>
2024-06-12 17:29:47 +0100

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -1,3 +1,4 @@
+import os
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -11,8 +12,8 @@ def send_email(sender_email, sender_password, recipient_email, subject, body):
     server = smtplib.SMTP(smtp_server, smtp_port)
     server.starttls()
 
-    # Log in to the SMTP server
-    server.login(sender_email, sender_password)
+    # Log in to the SMTP server using environment variable
+    server.login(sender_email, os.environ.get('EMAIL_PASSWORD'))
 
     # Create the email message
     msg = MIMEMultipart()
@@ -28,10 +29,9 @@ def send_email(sender_email, sender_password, recipient_email, subject, body):
     server.quit()
 
 # Example usage:
-sender_email = 'abudu.m100302@st.futminna.edu.ng'
-sender_password = 'Ab21du!?m'
-recipient_email = 'auralex99@gmail.com'
+sender_email = 'auralex99@gmail.com'
+recipient_email = 'abudu.m1700302@st.futminna.edu.ng.com'
 subject = 'Test Email'
 body = 'This is a test email sent from Python.'
 
-send_email(sender_email, sender_password, recipient_email, subject, body)
+send_email(sender_email, os.environ.get('EMAIL_PASSWORD'), recipient_email, subject, body)
